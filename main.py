b303707e67894bd2a4c62ba46cec31cc70accdfc
AndreyKosov <andrey.kosov.05@inbox.ru>
2024-11-20 21:53:34 +0500

    20.11.2024 1.0.2

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import uvicorn
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from src.app.application import lifespan
 from src.app.middlewares.globals import GlobalMiddleware
@@ -15,3 +16,10 @@ app = FastAPI(
 app.include_router(classifier_router)
 
 app.add_middleware(GlobalMiddleware)
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
